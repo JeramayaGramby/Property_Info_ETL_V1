69094813138b99e00d1d1f15f6f91ac99ff7b83c
Jeramaya Gramby <jeramayagramby@gmail.com>
2023-06-05 13:21:45 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# Importing all necessary libraries
+import os
+import pandas as pd
+import requests
+import warnings
+
+
+property_address = str(input(f'Enter the full Property Address with the zip code (Example: 423 Main Street 18015)'))
+
